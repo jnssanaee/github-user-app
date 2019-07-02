887fa6d66ef80f106b79ecbffb663f5fa61f83bd
lkt <jnssanaee@nate.com>
2019-07-02 16:54:48 +0900

    REFACT : input 입력 시 한글, 영문 구분 추가

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const input = document.querySelector("#input");
-const areaJson = document.querySelector("#areaJson");
-const tableTemplate = ({login, location, bio, url}) => {
-    return `
+const input = document.querySelector('#input');
+const areaJson = document.querySelector('#areaJson');
+const hangulPattern = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+const tableTemplate = ({ login, location, bio, url }) => {
+  return `
         <table class="table">
             <thead>
             <tr>
@@ -20,35 +21,40 @@ const tableTemplate = ({login, location, bio, url}) => {
             </tr>
             </tbody>
         </table> 
-    `
-}
+    `;
+};
 
-const renderTable = function(res){
-    areaJson.innerHTML = tableTemplate(res)
-}
+const renderTable = function(res) {
+  areaJson.innerHTML = tableTemplate(res);
+};
 
-const emptyData = function(){
-    areaJson.innerHTML = '<div class="arerNot">응답되는 주소가 없습니다.</div>';
-}
+const emptyData = function() {
+  const hangulCheck = hangulPattern.test(input.value);
+  if (hangulCheck)
+    return (areaJson.innerHTML =
+      '<div class="arerNot">한글프로필은 존재하지 않습니다. <br />영문이나 숫자, 영문 + 숫자 조합으로 다시 입력하시기 바랍니다.</div>');
+  areaJson.innerHTML = '<div class="arerNot">User Profile not found.</div>';
+};
 
-const handleApiRequest = function(e){
-    if(e.keyCode !== 13) {
-        return
-    }
+const handleApiRequest = function(e) {
+  if (e.keyCode !== 13) {
+    return;
+  }
 
-    const userName = e.target.value;
-    fetch('https://api.github.com/users/' + userName)
-        .then((res) => {
-            if (res.ok) {
-                res.json().then((res) => {
-                    console.log(typeof res)
-                    renderTable(res)
-                });
-            } else {
-              emptyData();
-            }
-        })
-        .catch(err => console.error(err));
-}
+  const userName = e.target.value;
+  fetch('https://api.github.com/users/' + userName)
+    .then((res) => {
+      if (res.ok) {
+        res.json().then((res) => {
+          console.log(typeof res);
+          renderTable(res);
+        });
+      } else {
+        emptyData();
+        //console.log(1);
+      }
+    })
+    .catch((err) => console.error(err));
+};
 
-input.addEventListener("keyup", handleApiRequest);
+input.addEventListener('keyup', handleApiRequest);
